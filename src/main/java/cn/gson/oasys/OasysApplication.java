2c2a9bd6178caa6a9000e8c4d6009d874101cdc5
jay-thinkpad <jay-thinkpad@qq.com>
2020-07-10 12:06:17 +0800

    jar改成war

--- src/main/java/cn/gson/oasys/OasysApplication.java
+++ src/main/java/cn/gson/oasys/OasysApplication.java
@@ -2,10 +2,20 @@ package cn.gson.oasys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+//使用jar方式部署
+//@SpringBootApplication
+//public class OasysApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(OasysApplication.class, args);
+//	}
+//}
 
+//使用war方式部署
 @SpringBootApplication
-public class OasysApplication {
+public class OasysApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OasysApplication.class, args);
