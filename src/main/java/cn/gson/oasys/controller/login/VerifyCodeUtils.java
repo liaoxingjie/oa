4e6a0c174abdc8ee6f9dc00c91cae4e2ad72af8f
jay-thinkpad <jay-thinkpad@qq.com>
2020-07-20 14:32:22 +0800

    试图解决linux上验证码图片不生成的问题2

--- src/main/java/cn/gson/oasys/controller/login/VerifyCodeUtils.java
+++ src/main/java/cn/gson/oasys/controller/login/VerifyCodeUtils.java
@@ -137,67 +137,94 @@ public final class VerifyCodeUtils {
 	 * @param code
 	 * @throws IOException
 	 */
+	// 尝试解决linux下不能生成验证码图片的问题
+//	public static void outputImage(int w, int h, OutputStream os, String code) throws IOException {
+//		System.setProperty("java.awt.headless", "true");//解决linux验证码图片不生成的问题
+//		int verifySize = code.length();
+//		BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+//		Random rand = new Random();
+//		Graphics2D g2 = image.createGraphics();
+//		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//		Color[] colors = new Color[5];
+//		Color[] colorSpaces = new Color[] { Color.WHITE, Color.CYAN, Color.GRAY, Color.LIGHT_GRAY, Color.MAGENTA,
+//				Color.ORANGE, Color.PINK, Color.YELLOW };
+//		float[] fractions = new float[colors.length];
+//		for (int i = 0; i < colors.length; i++) {
+//			colors[i] = colorSpaces[rand.nextInt(colorSpaces.length)];
+//			fractions[i] = rand.nextFloat();
+//		}
+//		Arrays.sort(fractions);
+//
+//		g2.setColor(Color.GRAY);// 设置边框色
+//		g2.fillRect(0, 0, w, h);
+//
+//		Color c = getRandColor(200, 250);
+//		g2.setColor(c);// 设置背景色
+//		g2.fillRect(0, 2, w, h - 4);
+//
+//		// 绘制干扰线
+//		Random random = new Random();
+//		g2.setColor(getRandColor(160, 200));// 设置线条的颜色
+//		for (int i = 0; i < 20; i++) {
+//			int x = random.nextInt(w - 1);
+//			int y = random.nextInt(h - 1);
+//			int xl = random.nextInt(6) + 1;
+//			int yl = random.nextInt(12) + 1;
+//			g2.drawLine(x, y, x + xl + 40, y + yl + 20);
+//		}
+//
+//		// 添加噪点
+//		float yawpRate = 0.05f;// 噪声率
+//		int area = (int) (yawpRate * w * h);
+//		for (int i = 0; i < area; i++) {
+//			int x = random.nextInt(w);
+//			int y = random.nextInt(h);
+//			int rgb = getRandomIntColor();
+//			image.setRGB(x, y, rgb);
+//		}
+//
+//		shear(g2, w, h, c);// 使图片扭曲
+//
+//		g2.setColor(getRandColor(100, 160));
+//		int fontSize = h - 4;
+//		Font font = new Font("Algerian", Font.ITALIC, fontSize);
+//		g2.setFont(font);
+//		char[] chars = code.toCharArray();
+//		for (int i = 0; i < verifySize; i++) {
+//			AffineTransform affine = new AffineTransform();
+//			affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1),
+//					(w / verifySize) * i + fontSize / 2, h / 2);
+//			g2.setTransform(affine);
+//			g2.drawChars(chars, i, 1, ((w - 10) / verifySize) * i + 5, h / 2 + fontSize / 2 - 10);
+//		}
+//
+//		g2.dispose();
+//		ImageIO.write(image, "jpg", os);
+//	}
 	public static void outputImage(int w, int h, OutputStream os, String code) throws IOException {
-		System.setProperty("java.awt.headless", "true");//解决linux验证码图片不生成的问题
-		int verifySize = code.length();
-		BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-		Random rand = new Random();
-		Graphics2D g2 = image.createGraphics();
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		Color[] colors = new Color[5];
-		Color[] colorSpaces = new Color[] { Color.WHITE, Color.CYAN, Color.GRAY, Color.LIGHT_GRAY, Color.MAGENTA,
-				Color.ORANGE, Color.PINK, Color.YELLOW };
-		float[] fractions = new float[colors.length];
-		for (int i = 0; i < colors.length; i++) {
-			colors[i] = colorSpaces[rand.nextInt(colorSpaces.length)];
-			fractions[i] = rand.nextFloat();
-		}
-		Arrays.sort(fractions);
 
-		g2.setColor(Color.GRAY);// 设置边框色
-		g2.fillRect(0, 0, w, h);
+		//生成一张图片
+		BufferedImage image = new BufferedImage(65, 25, BufferedImage.TYPE_INT_RGB);    // 实例化BufferedImage
+		//获取一根笔对象
+		Graphics g = image.getGraphics();
+		Color c = new Color(200, 200, 255);                                             // 验证码图片的背景颜色
+		g.setColor(c);
+		g.fillRect(0, 0, 65, 25);
+		Font font = new Font("楷体", 1, 20);
+		g.setFont(font);// 图片的边框
 
-		Color c = getRandColor(200, 250);
-		g2.setColor(c);// 设置背景色
-		g2.fillRect(0, 2, w, h - 4);
+		Random r = new Random();
 
-		// 绘制干扰线
-		Random random = new Random();
-		g2.setColor(getRandColor(160, 200));// 设置线条的颜色
-		for (int i = 0; i < 20; i++) {
-			int x = random.nextInt(w - 1);
-			int y = random.nextInt(h - 1);
-			int xl = random.nextInt(6) + 1;
-			int yl = random.nextInt(12) + 1;
-			g2.drawLine(x, y, x + xl + 40, y + yl + 20);
+		//画干扰线
+		for (int i = 0; i < 5; i++) {
+			c = new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+			g.setColor(c);
+			g.drawLine(r.nextInt(65), r.nextInt(25), r.nextInt(65), r.nextInt(25));
 		}
 
-		// 添加噪点
-		float yawpRate = 0.05f;// 噪声率
-		int area = (int) (yawpRate * w * h);
-		for (int i = 0; i < area; i++) {
-			int x = random.nextInt(w);
-			int y = random.nextInt(h);
-			int rgb = getRandomIntColor();
-			image.setRGB(x, y, rgb);
-		}
+		g.setColor(new Color(r.nextInt(88), r.nextInt(210), r.nextInt(150)));       // 随机一个颜色
+		g.drawString(code, 10, 18);                              // 画出字符
 
-		shear(g2, w, h, c);// 使图片扭曲
-
-		g2.setColor(getRandColor(100, 160));
-		int fontSize = h - 4;
-		Font font = new Font("Algerian", Font.ITALIC, fontSize);
-		g2.setFont(font);
-		char[] chars = code.toCharArray();
-		for (int i = 0; i < verifySize; i++) {
-			AffineTransform affine = new AffineTransform();
-			affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1),
-					(w / verifySize) * i + fontSize / 2, h / 2);
-			g2.setTransform(affine);
-			g2.drawChars(chars, i, 1, ((w - 10) / verifySize) * i + 5, h / 2 + fontSize / 2 - 10);
-		}
-
-		g2.dispose();
 		ImageIO.write(image, "jpg", os);
 	}
 
